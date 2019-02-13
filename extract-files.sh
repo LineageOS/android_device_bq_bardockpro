959ae4d70a797c88f062def0aef10b1281064bd3
Quallenauge <Hamsi2k@freenet.de>
2019-02-13 15:01:15 +0100

    extract-files: Use commonized version.
    Use common extract-files.sh script in ../msm8953-common.
    
    Change-Id: I6c84e08395ce29dffd373df84acc08f4bcc8704f

--- extract-files.sh
+++ extract-files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,49 +15,18 @@
 # limitations under the License.
 #
 
-set -e
-
-DEVICE=bardockpro
-VENDOR=bq
-
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# default to not sanitizing the vendor folder before extraction
-clean_vendor=false
-
-while [ "$1" != "" ]; do
-    case $1 in
-        -p | --path )           shift
-                                SRC=$1
-                                ;;
-        -s | --section )        shift
-                                SECTION=$1
-                                clean_vendor=false
-                                ;;
-        -c | --clean-vendor )   clean_vendor=true
-                                ;;
-    esac
-    shift
-done
-
-if [ -z "$SRC" ]; then
-    SRC=adb
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+	return
 fi
 
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
+set -e -u -o pipefail
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+# Required!
+export DEVICE=bardockpro
+export VENDOR=bq
 
-"$MY_DIR"/setup-makefiles.sh
+MY_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+
+${MY_DIR}/../msm8953-common/extract-files.sh "$@"
