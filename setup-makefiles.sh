2fc18e3a582b437072d01d775672a26b2b2e4064
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 20:43:12 +0200

    bardockpro: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 #
-# Copyright (C) 2017-2018 The LineageOS Project4
+# Copyright (C) 2018-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e -u -o pipefail
 
-# Required!
 export DEVICE=bardockpro
+export DEVICE_COMMON=msm8953-common
 export VENDOR=bq
 
-MY_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
-
-${MY_DIR}/../msm8953-common/setup-makefiles.sh "$@"
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
