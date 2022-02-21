ae5bb24d42984fd4842aaec3b1cb80f280b3b419
Quallenauge <Hamsi2k@freenet.de>
2022-02-21 20:24:00 +0100

    props: Move dalvik properties to specific device tree.
    Change-Id: Ie8a1ed5edcc9356e72bcab2c747a3ebda3be9025

--- device.mk
+++ device.mk
@@ -18,3 +18,11 @@ $(call inherit-product, vendor/bq/bardockpro/bardockpro-vendor.mk)
 
 # setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=8m
